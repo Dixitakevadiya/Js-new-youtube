7c2451e00cecd1c9e0570bc950e820c920b102e5
Dixita Kevadiya <85424127+Dixitakevadiya@users.noreply.github.com>
2025-04-16 09:59:14 +0000

    data types

---
+++ 01_basis/01_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert('test') we are using node.js not browser
+
+let name = "sagar"
+let age = 25
+let isLogged = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone val
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
--- 01_basis/test.js
+++ 01_basis/test.js
@@ -1,1 +1,4 @@
-console.log('Hello')
+// https://262.ecma-international.org/15.0/index.html
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+console.log('Hello');
