5cc334739e007a7308439c303ea29fc34f0d7c62
Dixita Kevadiya <85424127+Dixitakevadiya@users.noreply.github.com>
2025-04-16 09:41:37 +0000

    commit 1

---
+++ 01_basis/01_variable.js
@@ -0,0 +1,14 @@
+const account_id = 123;
+let account_email = "test@google.com"
+var pwd ="1232"
+accountCity = "surat"
+
+account_email = "hgufhf"
+// account_id = 5677
+
+console.log('account_id: ',account_id);
+console.log('account_id: ',account_email);
+console.log('accountCity: ',accountCity);
+console.table({"account_id":account_id, "account_email":account_email, "pwd": pwd});
+
+
