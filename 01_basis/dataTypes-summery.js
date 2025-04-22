5c62e9939ef488fdcdcdcdca97996702a576bdd3
Dixita Kevadiya <85424127+Dixitakevadiya@users.noreply.github.com>
2025-04-22 10:16:24 +0000

    data Type

---
+++ 01_basis/dataTypes-summery.js
@@ -0,0 +1,45 @@
+// Primitive Type
+
+// 1. String
+// 2. Null
+// 3. undefined
+// 4. Number
+// 5. Boolean
+// 6. bigint
+// 7. symbol
+
+const score = 100
+const scoreVal = 100.3
+const isLoggin = false
+const outTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId)
+
+const bigNumber = 12364456876876746n
+
+
+// Non primtive type[reference type]
+
+// 1. array
+// 2. object
+// 3. functions
+
+// all non premetive typeof function means object function
+ 
+
+const test = ["abc","xyz","test"];
+
+let myObj = {
+    name: "hitesh",
+    age: 25
+}
+
+const myFun = function(){
+    console.log('my function');
+}
+
+console.log(typeof myObj)
