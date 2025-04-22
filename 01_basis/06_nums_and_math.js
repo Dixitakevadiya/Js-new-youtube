83fc7b944e03f3b102ed84b03cf5c20a442cb6cd
Dixita Kevadiya <85424127+Dixitakevadiya@users.noreply.github.com>
2025-04-22 11:44:28 +0000

    Math

---
+++ 01_basis/06_nums_and_math.js
@@ -0,0 +1,17 @@
+// =====================Number====================
+
+const score = 400
+// console.log(score);
+const balace = new Number(400.87)
+// console.log(balace);
+
+// console.log(balace.toPrecision(4));
+
+const hunder = 40000000
+// console.log(hunder.toLocaleString());
+
+// =====================Maths====================
+console.log(Math.abs(-4));
+console.log(Math.random());
+
+
