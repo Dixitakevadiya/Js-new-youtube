65b11b8d48eb64f6f9b78a195a4d08c0cc767fbf
Dixita Kevadiya <85424127+Dixitakevadiya@users.noreply.github.com>
2025-05-12 11:58:10 +0000

    DOM

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,12 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
+
 // const newNumssss = []
 // const newfor = myNumers.forEach( (num) => {
 //     newNumssss.push(num + 10)     
 // })
+
 // console.log(newNumssss);
 
 const newNums = myNumers
